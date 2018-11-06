1cd87082dbcfcf0bb6ea25ba447d492e8407f1ce
Oliver Ponder <oliver.ponder@gmail.com>
2018-11-06 14:41:41 +0800

    Add a way to copy new private key to clipboard.

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c6765a3fa4cef8dddd32";
+/******/ 	var hotCurrentHash = "c9a2befe208dc0895d8c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
